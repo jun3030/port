af44cf5c9770622e387226a6ed07ad2505889e06
EC2 Default User <ec2-user@ip-172-31-11-81.us-east-2.compute.internal>
2020-02-20 11:34:58 +0000

    viewの条件わけ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
  
-  
+  def current_user?(user)
+    user == current_user
+  end
   
   
   
