ed4fb1f71c73e1f34293326d7f8764031399c475
EC2 Default User <ec2-user@ip-172-31-11-81.us-east-2.compute.internal>
2020-02-20 10:06:33 +0000

    save

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,6 +1,23 @@
 class UrlsController < ApplicationController
+    before_action :set_user, only: [:users_imaeg_and_video_url]
     
     def users_imaeg_and_video_url
-    
+      url_params.each do |id, item|
+        url_models_id = Url.find(id)
+        url_models_id.update(item)
+      end
+      
     end
+    
+    private
+    
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+    
+    def url_params
+      params.require(:user).permit(urls: [:public])[:urls]
+    end
+    
+    
 end
