2da2060b273d6926e7762061f5eaf8e0014f8716
EC2 Default User <ec2-user@ip-172-31-11-81.us-east-2.compute.internal>
2020-03-08 04:37:17 +0000

    条件指定もういい　触りたくない

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,9 @@ class PostsController < ApplicationController
   before_action :all, only: [:users_posts, :users_create_posts, :users_edit_posts, :index]
   
   def index
-    
+   @users = User.all
     # Post.find(7).activity_area.include?("#{params[:q][:posts_activity_area_cont]}".gsub(/[\[\]\"]/, ""))
-    @users = User.all
+  
     @search = User.includes(:posts).ransack(params[:q])  #追加
     
   if params[:q].present?
@@ -15,7 +15,9 @@ class PostsController < ApplicationController
      part_array = Post.all.pluck(:recruitment_part).select { |item| item.include?("#{params[:q][:posts_recruitment_part_matches_any]}") }
      ganre_array = Post.all.pluck(:band_genre).select { |item| item.include?("#{params[:q][:posts_band_genre_matches_any]}") }
      @posts = Post.all.where(activity_area: area_array).where(recruitment_part: part_array).where(band_genre: ganre_array)
+   
   else
+    
      @posts = Post.all
   end
   
